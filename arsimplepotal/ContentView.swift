e25b98cf1460f8bff03ed2377a7bcdb90e19c985
ynaga <ynaga@mail.com>
2022-01-20 06:55:32 +0900

    Initial commit.

---
+++ arsimplepotal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  arsimplepotal
+//
+//  Created by Yasuhito NAGATOMO on 2022/01/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ arsimplepotal/arsimplepotalApp.swift
@@ -0,0 +1,17 @@
+//
+//  arsimplepotalApp.swift
+//  arsimplepotal
+//
+//  Created by Yasuhito NAGATOMO on 2022/01/20.
+//
+
+import SwiftUI
+
+@main
+struct arsimplepotalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
